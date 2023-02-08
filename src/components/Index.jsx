5d145fba9b93cddb8b08002c8c33abfe7609169e
daffaRadhitya <111202012648@mhs.dinus.ac.id>
2023-02-08 13:00:25 +0700

    type effect

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import gambar from "../assets/foto.png"
+import { Typewriter } from 'react-simple-typewriter'
 
 function Index() {
   return (
@@ -8,7 +9,19 @@ function Index() {
       <Container>
       <Row style={{justifyContent:'center'}} >
         <Col lg={1}></Col>
-        <Col  className="title" lg={5} md={12} sm={12}><h1>Hello, i'm Daffa</h1><h1>I'm a passionate <span className="title" style={{color:'brown',textDecoration:'underline'}}>Frontend Developer</span></h1></Col>
+        <Col  className="title" lg={5} md={12} sm={12}><h1>Hello, i'm Daffa</h1><h1>I'm a passionate <br /> <span style={{ color:'brown',textDecoration:'underline'}}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={['Computer Science Student', 'Frontend Developer', 'UI/UX Enthusiasm']}
+            loop={5}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            
+          />
+        </span></h1></Col>
         <Col lg={5}  md={6} style={{zIndex:'1'}}><img className="gbrSaya"  src={gambar} width={500} /> </Col>
         <Col lg={1}></Col>
       </Row>
